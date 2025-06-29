75eefe0c22db7b94cca28758dd93a912b74be70c
Roman Khadraliev <Romch33tos@yandex.ru>
2025-06-29 15:24:53 +0700

    Добавлено меню

--- 15.py
+++ 15.py
@@ -10,7 +10,16 @@ class FifteenPuzzle:
     self.root.resizable(width=False, height=False)
     self.grid_size = 4
     self.tiles = list(range(1, self.grid_size * self.grid_size)) + [None]
-
+    
+    self.setup_menu()
+    
+  def setup_menu(self):
+    menubar = tk.Menu(self.root)
+    game_menu = tk.Menu(menubar, tearoff=0)
+    game_menu.add_command(label="Новая игра", command=self.restart_game)
+    menubar.add_cascade(label="Игра", menu=game_menu)
+    self.root.config(menu=menubar)
+    
 if __name__ == "__main__":
   ctk.set_appearance_mode("system")
   ctk.set_default_color_theme("blue")
